2ea939a7c080a0d97c00cbc878bdd1a8d94e1d1a
He Qiujie <patrick233@qq.com>
2021-11-19 16:55:06 +0800

    update

--- s8_3.cpp
+++ s8_3.cpp
@@ -6,5 +6,9 @@
 int main(int argc, char const *argv[]) {
     int a[10] = {2, 5, 78, 4, 6, 3, 89, 12, 15, 34};
     std::priority_queue<std::pair<int, int>> h;
-    for (int i = 0; i < 10)
+    for (int i = 0; i < 10; i++)
+        h.push(std::make_pair(a[i], i + 1));
+    std::cout << "max=" << h.top().first << "\nmaxindex=" << h.top().second
+              << '\n';
+    return 0;
 }
---
+++ s8_4.cpp
@@ -0,0 +1,8 @@
+// s8_4.cpp
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    bool prime[105] = {false};
+
+    return 0;
+}
