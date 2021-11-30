0ead1db35838cbb81c53d1d6927e25503840e386
He Qiujie <patrick233@qq.com>
2021-11-30 20:50:40 +0800

    update

---
+++ s12_4.cpp
@@ -0,0 +1,18 @@
+// 12_4.cpp
+#include <iostream>
+
+double average(int *array, int (*coloum)[4]);
+double maximum(int *array, int (*coloum)[4]);
+double mininum(int *array, int (*coloum)[4]);
+
+int main(int argc, char const *argv[]) {
+    int a[3][4] = {{2, 3, 4, 5}, {12, 13, 14, 15}, {1, 2, 3, 4}};
+
+    return 0;
+}
+
+double average(int *array, int (*coloum)[4]) {}
+
+double maximum(int *array, int (*coloum)[4]) {}
+
+double mininum(int *array, int (*coloum)[4]) {}
