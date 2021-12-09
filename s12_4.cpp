37ac46ef2094d727902107e131e909631e47e12c
He Qiujie <patrick233@qq.com>
2021-12-09 11:44:57 +0800

    update

--- s12_4.cpp
+++ s12_4.cpp
@@ -1,18 +1,43 @@
 // 12_4.cpp
 #include <iostream>
 
-double average(int *array, int (*coloum)[4]);
-double maximum(int *array, int (*coloum)[4]);
-double mininum(int *array, int (*coloum)[4]);
+double average(int (*coloum)[4]);
+int maximum(int *array);
+int mininum(int *array);
 
 int main(int argc, char const *argv[]) {
     int a[3][4] = {{2, 3, 4, 5}, {12, 13, 14, 15}, {1, 2, 3, 4}};
-
+    std::cout << "ave=" << average(a) << "\nmax=" << maximum(a[0])
+              << "\nmin=" << mininum(a[0]) << '\n';
     return 0;
 }
 
-double average(int *array, int (*coloum)[4]) {}
+double average(int (*coloum)[4]) {
+    double sum = 0;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            sum += *(*(coloum + i) + j);
+    return sum / 12;
+}
 
-double maximum(int *array, int (*coloum)[4]) {}
+int maximum(int *array) {
+    int *tmp = array;
+    int ret = *array;
+    while (tmp != (array + 11)) {
+        if (ret < *tmp)
+            ret = *tmp;
+        tmp++;
+    }
+    return ret;
+}
 
-double mininum(int *array, int (*coloum)[4]) {}
+int mininum(int *array) {
+    int *tmp = array;
+    int ret = *array;
+    while (tmp != (array + 11)) {
+        if (ret > *tmp)
+            ret = *tmp;
+        tmp++;
+    }
+    return ret;
+}
--- s13_4.cpp
+++ s13_4.cpp
@@ -5,15 +5,20 @@
 int fun(char *str, char *substr);
 
 int main(int argc, char const *argv[]) {
-
+    char str[100], substr[100];
     std::cout << "Input string:";
-
+    std::cin.getline(str, 100);
+    std::cout << "Input substring:";
+    std::cin >> substr;
+    std::cout << "n=" << fun(str, substr) << '\n';
     return 0;
 }
 
 int fun(char *str, char *substr) {
     int ret = 0;
-    std::string s(str), subs(substr);
-    size_t found = std::string::npos;
-    while ((found == s.find(subs)))
+    std::string s(str);
+    size_t found = -1;
+    while ((found = s.find(substr, found + 1)) != std::string::npos)
+        ret++;
+    return ret;
 }
